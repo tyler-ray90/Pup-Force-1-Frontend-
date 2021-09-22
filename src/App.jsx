faa9f0a687bb1dca6ad02cdabb25518c53c34f98
tyler-ray90 <tyler_ray98@yahoo.com>
2021-09-21 21:46:11 -0400

    Added the search bar to homepage

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,16 @@ import {Route, Switch} from 'react-router-dom';
 //Imported Pages
 import HomePage from "pages/HomePage";
 
+//Imported Components
+import Search from 'components/Search/Search';
+
+
 const App = () => {
     //! BrowserRouter already in index.jsx
     return <div className="App">;
-    <header>This is the header</header>
+    <nav>This is the nav</nav>
+    <Search/>
+
         <Switch>
         <Route exact path = "/" render={(props) => 
         <HomePage/>
---
+++ src/components/Search/Search.js
@@ -0,0 +1,13 @@
+function Search() {
+    return (
+        <section>
+            <input 
+            type="text" 
+            placeholder="Can my pet have..."
+            className="searchbox"
+            />
+        </section>
+    )
+};
+
+export default Search;
