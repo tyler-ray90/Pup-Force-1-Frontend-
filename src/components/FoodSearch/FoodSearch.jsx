1fb31cb91ad70dc0a3e823cb60cc3f202aeb611d
irackson <nasirus@protonmail.com>
2021-09-22 19:37:06 -0400

    sucessful API call

--- src/components/FoodSearch/FoodSearch.jsx
+++ src/components/FoodSearch/FoodSearch.jsx
@@ -3,18 +3,16 @@ import { useState } from 'react';
 
 import './FoodSearch.scss';
 
-const FoodSearch = () => {
+const FoodSearch = ({ data, addOrUpdate }) => {
     const [isListOpen, setIsListOpen] = useState(false);
     const [pet, setPet] = useState('Cats');
-    const [food, setFood] = useState('');
+    // const [data, setData] = useState('');
 
     const toggleListOpen = () => {
         isListOpen ? setIsListOpen(false) : setIsListOpen(true);
-    }
+    };
 
-
-
-    return (
+    const loaded = () => (
         <div className="foodSearch">
             <h2 className="foodSearch__h2">Can</h2>
             {/* <select className="foodSearch__dropdown__select" id="pets">
@@ -22,40 +20,48 @@ const FoodSearch = () => {
                     <option className="foodSearch__option" value="Dogs">Dogs</option>
                 </select>  */}
             <div className="foodSearch__dropdown">
-                {!isListOpen && 
-                    <p className="foodSearch__dropdown__placeholder" 
-                        onClick={toggleListOpen}>
+                {!isListOpen && (
+                    <p
+                        className="foodSearch__dropdown__placeholder"
+                        onClick={toggleListOpen}
+                    >
                         {pet}
                     </p>
-                }
-                {isListOpen && 
+                )}
+                {isListOpen && (
                     <ul className="foodSearch__dropdown__list">
-                        <li 
-                            className="foodSearch__dropdown__list__item" 
+                        <li
+                            className="foodSearch__dropdown__list__item"
                             onClick={() => {
                                 toggleListOpen();
                                 setPet('Cats');
-                            }}>
+                            }}
+                        >
                             Cats
                         </li>
-                        <li 
-                            className="foodSearch__dropdown__list__item" 
+                        <li
+                            className="foodSearch__dropdown__list__item"
                             onClick={() => {
                                 toggleListOpen();
-                                setPet('Dogs')
-                            }}>
+                                setPet('Dogs');
+                            }}
+                        >
                             Dogs
                         </li>
                     </ul>
-                }
+                )}
             </div>
             <div className="foodSearch__grayLine"></div>
             <h2 className="foodSearch__h2">Eat</h2>
-            <AutoComplete placeholder="Onions"/>
+            <AutoComplete placeholder="Onions" />
             <div className="foodSearch__grayLine"></div>
-            <button className='foodSearch__button'>?</button>
+            <button className="foodSearch__button">?</button>
         </div>
-    )
-}
+    );
 
-export default FoodSearch
+    const loading = () => <div>loading...</div>;
+
+    return data ? loaded() : loading();
+};
+
+export default FoodSearch;
