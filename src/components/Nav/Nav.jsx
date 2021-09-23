dfff55462491be90cc323b503f3b8f14854aeb80
tjconti12 <78386402+tjconti12@users.noreply.github.com>
2021-09-23 13:04:24 -0400

    update nav styles and include logo

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -5,6 +5,8 @@ import * as AiIcons from 'react-icons/ai';
 import './Nav.scss';
 import { IconContext } from 'react-icons';
 
+import logo from '../../resources/images/logo.png';
+
 import { SidebarData } from './SidebarData';
 
 function Nav() {
@@ -37,7 +39,11 @@ function Nav() {
                         </li>
                     )
                 })}
-             </ul>   
+             </ul>  
+             <div className="nav-logo">
+                <img className="nav-logo-img" src={logo} alt="logo of dog sleeping"/>
+                <p className="nav-logo-text">Can Spot Eat</p>
+            </div> 
             </nav>
             </IconContext.Provider>
         </>
